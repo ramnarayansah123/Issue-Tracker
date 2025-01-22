d0dde313c5972f1b9b56287d0bfa8735a4710b75
Ramnarayan <ramnarayansah180@gmail.com>
2025-01-22 13:21:56 +0545

    post api with database done

--- app/issue/new/page.tsx
+++ app/issue/new/page.tsx
@@ -1,16 +1,25 @@
 "use client"
 
 import Navbar from "@/app/component/Navbar";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button,  TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import {useForm} from "react-hook-form"
 
+
+interface IssueForm{
+    tittle:string;
+    description:string;
+}
 export default function NewIssuePage() {
+    
     return (
         <>
         <Navbar></Navbar>
             <div className="max-W-xl space-y-3">
             <TextField.Root placeholder="Search the docs…">
                 </TextField.Root>
-                <TextArea placeholder="Reply to comment…"/>
+                <SimpleMDE placeholder="Reply to comment…"/>
                 <Button>Submit New Issue</Button>
             </div>
         </>
